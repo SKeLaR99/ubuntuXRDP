37643aee9e8eb96e8ad9c664bfb305e8420feb26
SKeLaR99 <jandelzm+2@gmail.com>
2026-06-04 20:09:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-service dbus start
-pulseaudio --start --system --disallow-exit --disable-shm
+mkdir -p /var/run/dbus
+dbus-daemon --system
+
+service xrdp-sesman start
 service xrdp start
 
-mkdir -p /tmp/.X11-unix
-chmod 1777 /tmp/.X11-unix
+echo ""
+echo "================================="
+echo "XRDP READY"
+echo "Username: codespace"
+echo "Password: codespace"
+echo "Port: 3389"
+echo "================================="
+echo ""
 
-tail -f /var/log/xrdp-sesman.log
+tail -f /dev/null
